fac0a04ab92dfa80f0a95d4461fe524943b4b48e
GEkaterina <31741790+GEkaterina@users.noreply.github.com>
2020-12-15 17:20:49 +0300

    .

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo512.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          airbnb
+        </p>
+        <a
+          className="App-link"
+          href="https://www.airbnb.ru/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Official site
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
